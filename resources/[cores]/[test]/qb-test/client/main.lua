d4ab90c060559915e8b166256293b9f5842e59a7
KASH <kashvdmail@gmail.com>
2019-06-27 16:31:43 +0200

    placeholder

--- resources/[cores]/[test]/qb-test/client/main.lua
+++ resources/[cores]/[test]/qb-test/client/main.lua
@@ -6,3 +6,23 @@ Citizen.CreateThread(function()
         Citizen.Wait(200)
     end
 end)
+--[[
+RegisterNetEvent('QBCore:TestMenu')
+AddEventHandler('QBCore:TestMenu', function()
+	local elms = {}
+	table.insert(elms, {label = "Test 1", onclick = function()
+			print("TEST 1 TRIGGERED")
+		end, onchange = function()
+			print("CHANGED TO TEST 1")
+		end
+	})
+	table.insert(elms, {label = "Test 2", onclick = function()
+			print("TEST 2 TRIGGERED")
+		end, onchange = function()
+			print("CHANGED TO TEST 2")
+		end
+	})
+	QBCore.Functions.OpenMenu("Title", "Subtitle", elms)
+end)
+]]
+
