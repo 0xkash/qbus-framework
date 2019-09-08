37d8f383ed07da3f37782347858e67d2fc5c06fa
KASH <kashvdmail@gmail.com>
2019-09-08 16:10:48 +0200

    Removed: qb-hud, Moved: Intenvory
    Removed qb hud for now and moved the inventory to another directory

--- resources/[cores]/[qbus]/qb-hud/config.lua
+++
@@ -1,8 +0,0 @@
-Config = {}
-
-Config.Money = {}
-Config.Money.Prefix = "€"
-Config.Money.HUDEnable = true -- show moneyhud
-Config.Money.HUDOn = true
-Config.Money.DrawLocations = {['cash'] = {x = 1.422, y = 0.6122, prefix = "~g~"..Config.Money.Prefix.." ~w~", alpha = 250, show = true}, ['bank'] = {x = 1.422, y = 0.6422, prefix = "~b~"..Config.Money.Prefix.." ~w~", alpha = 250, show = true}} -- Draw locations for money, is HUDEnable is false it doesn't matter, add only the moneytypes you want to show and make sure they are in the MoneyTypes table, set show to false if you want to disable it for showing in commands (/cash /bank etc.)
-Config.Money.MoneyTimeOut = 5 -- amount much time money hud disapear after a change (in seconds), set to -1 to keep on
