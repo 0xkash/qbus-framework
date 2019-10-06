ed4ab783995a72b68a83f77e3efd2082fa6ec28b
KASH <kashvdmail@gmail.com>
2019-10-06 13:20:12 +0200

    Added: new loading screen

---
+++ resources/[standalone]/loadscreen/README.md
@@ -0,0 +1,17 @@
+﻿v1
+
+Loadingscreen
+
+##Installation
+
+1. Put "vv-loader" into your server.cfg
+2. Enjoy!
+
+For Support: https://discord.gg/WZrArc8
+ 
+Credits
+
+^Mika
+#4672
+JanKrb | Dev
+#6964
---
+++ resources/[standalone]/loadscreen/__resource.lua
@@ -0,0 +1,13 @@
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+loadscreen 'index.html'
+
+
+files {
+    'index.html',
+    'stylesheet.css',
+    'app.js',
+    'music/music.ogg',
+    'imgs/logo.png'
+}
