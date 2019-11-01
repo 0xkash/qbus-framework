5f4442bf39f72cc3dbbf3e915d30a8ae296a5901
KASH <kashvdmail@gmail.com>
2019-11-01 19:55:49 +0100

    Emotes :)

---
+++ resources/[standalone]/animations/server/main.lua
@@ -0,0 +1,10 @@
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+QBCore.Commands.Add("am", "Toggle animatie menu", {}, false, function(source, args)
+	TriggerClientEvent('animations:client:ToggleMenu', source)
+end)
+
+QBCore.Commands.Add("a", "Gebruik een animatie, voor animatie lijst doe /em", {name = "naam", help = "Emote naam"}, true, function(source, args)
+	TriggerClientEvent('animations:client:EmoteCommandStart', source, args)
+end)
