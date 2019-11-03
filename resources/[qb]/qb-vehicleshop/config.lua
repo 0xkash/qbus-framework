3439725220f9a9795e19ad593c932efa9308b58e
s0me1k <jurre.met@gmail.com>
2019-11-03 16:57:45 +0100

    Vshop

--- resources/[qb]/qb-vehicleshop/config.lua
+++ resources/[qb]/qb-vehicleshop/config.lua
@@ -1,7 +1,7 @@
 QB = {}
 
 QB.VehicleShops = {
-    {x = -56.71, y = -1096.65, z = 26.42}
+    {x = -56.71, y = -1096.65, z = 25.44}
 }
 
 QB.GarageLabel = {
@@ -14,49 +14,49 @@ QB.DefaultGarage = "centralgarage"
 
 QB.ShowroomVehicles = {
     [1] = {
-        coords = {x = -45.65, y = -1093.66, z = 26.42, h = 69.5},
+        coords = {x = -45.65, y = -1093.66, z = 25.44, h = 69.5},
         defaultVehicle = "adder",
         chosenVehicle = "adder",
         inUse = false,
     },
     [2] = {
-        coords = {x = -48.27, y = -1101.86, z = 26.42, h = 294.5},
+        coords = {x = -48.27, y = -1101.86, z = 25.44, h = 294.5},
         defaultVehicle = "schafter2",
         chosenVehicle = "schafter2",
         inUse = false,
     },
     [3] = {
-        coords = {x = -39.6, y = -1096.01, z = 26.42, h = 66.5},
+        coords = {x = -39.6, y = -1096.01, z = 25.44, h = 66.5},
         defaultVehicle = "comet2",
         chosenVehicle = "comet2",
         inUse = false,
     },
     [4] = {
-        coords = {x = -51.21, y = -1096.77, z = 26.42, h = 254.5},
+        coords = {x = -51.21, y = -1096.77, z = 25.44, h = 254.5},
         defaultVehicle = "schwarzer2",
         chosenVehicle = "schwarzer2",
         inUse = false,
     },
     [5] = {
-        coords = {x = -40.18, y = -1104.13, z = 26.42, h = 338.5},
+        coords = {x = -40.18, y = -1104.13, z = 25.44, h = 338.5},
         defaultVehicle = "t20",
         chosenVehicle = "t20",
         inUse = false,
     },
     [6] = {
-        coords = {x = -43.31, y = -1099.02, z = 26.42, h = 52.5},
+        coords = {x = -43.31, y = -1099.02, z = 25.44, h = 52.5},
         defaultVehicle = "bati",
         chosenVehicle = "bati",
         inUse = false,
     },
     [7] = {
-        coords = {x = -50.66, y = -1093.05, z = 26.42, h = 222.5},
+        coords = {x = -50.66, y = -1093.05, z = 25.44, h = 222.5},
         defaultVehicle = "bati",
         chosenVehicle = "bati",
         inUse = false,
     },
     [8] = {
-        coords = {x = -44.28, y = -1102.47, z = 26.42, h = 298.5},
+        coords = {x = -44.28, y = -1102.47, z = 25.44, h = 298.5},
         defaultVehicle = "bati",
         chosenVehicle = "bati",
         inUse = false,
