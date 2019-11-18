241965122fc518d00a3207c3f896b9787964124c
KASH <kashvdmail@gmail.com>
2019-11-18 22:36:35 +0100

    keypad storerobbery

--- resources/[qb]/qb-storerobbery/server/main.lua
+++ resources/[qb]/qb-storerobbery/server/main.lua
@@ -2,7 +2,9 @@ QBCore = nil
 
 TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
---CODE
+local SafeCodes = {
+    [1] = math.random(1000, 9999)
+}
 
 RegisterServerEvent('qb-storerobbery:server:takeMoney')
 AddEventHandler('qb-storerobbery:server:takeMoney', function()
@@ -19,10 +21,22 @@ AddEventHandler('qb-storerobbery:server:setRegisterStatus', function(register)
     Config.Registers[register].time     = Config.resetTime
 end)
 
+RegisterServerEvent('qb-storerobbery:server:setSafeStatus')
+AddEventHandler('qb-storerobbery:server:setSafeStatus', function(safe)
+    TriggerClientEvent('qb-storerobbery:client:setSafeStatus', -1, safe, true)
+    Config.Safes[safe].robbed = true
+end)
+
+RegisterServerEvent('qb-storerobbery:server:SafeReward')
+AddEventHandler('qb-storerobbery:server:SafeReward', function(safe)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    Player.Functions.AddMoney('cash', math.random(5000, 9000))
+end)
+
 Citizen.CreateThread(function()
     while true do
         for k, v in pairs(Config.Registers) do
-
             if Config.Registers[k].time > 0 and (Config.Registers[k].time - Config.tickInterval) >= 0 then
                 Config.Registers[k].time = Config.Registers[k].time - Config.tickInterval
             else
@@ -35,6 +49,14 @@ Citizen.CreateThread(function()
     end
 end)
 
+QBCore.Functions.CreateCallback('qb-storerobbery:server:isCombinationRight', function(source, cb, safe)
+    cb(SafeCodes[safe])
+end)
+
 QBCore.Functions.CreateCallback('qb-storerobbery:server:getRegisterStatus', function(source, cb)
     cb(Config.Registers)
+end)
+
+QBCore.Functions.CreateCallback('qb-storerobbery:server:getRegisterStatus', function(source, cb)
+    cb(Config.Safes)
 end)
