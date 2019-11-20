3df817349d68b7beea930ffb4bd5699034737ac1
KASH <kashvdmail@gmail.com>
2019-11-20 18:51:21 +0100

    makelaar iets

--- resources/[qb]/qb-housesbackup/config.lua
+++ resources/[qb]/qb-housesbackup/config.lua
@@ -2,7 +2,44 @@ Config = Config or {}
 
 Config.MinZOffset = 30
 
-Config.Houses = {}
+Config.Houses = {
+	["mirrorpark01"] = {
+		coords = { 
+			enter 	= { x = 887.36,  y = -607.62,  z = 57.25,   h = 318.0 },
+			cam 	= { x = 895.59,  y = -590.12,  z = 62.67, 	h = 135.00, yaw = -10.00},
+		}, 
+		owned = false, 
+		price = 200000,
+		locked = true,
+		adress = "01 Mirror Park",
+		tier = 1,
+		decorations = {},
+	},
+	["mirrorpark02"] = {
+		coords = { 
+			enter = { x = 919.99, y = -570.08, z = 57.7,   h = 318.0 },
+			cam = {x = 931.03, y = -577.15, z = 62.95, h = 35.5, yaw = -10.00}
+		}, 
+		owned = false, 
+		price = 250000,
+		locked = true,
+		adress = "02 Mirror Park",
+		tier = 2,
+		decorations = {},
+	},
+	["spanishave01"] = {
+		coords = { 
+			enter 	= { x = -113.45, y = 985.7, z = 235.75, h = 290.5},
+			cam 	= { x = -140.97, y = 981.44, z = 239.32, h = 281.5, yaw = -10.00},
+		}, 
+		owned = false, 
+		price = 750000,
+		locked = true,
+		adress = "01 Spanish Ave",
+		tier = 3,
+		decorations = {},
+	},
+}
 
 Config.Furniture = {
 	["sofas"] = {
