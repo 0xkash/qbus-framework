129253d6b6ec3511d27ec0c8f519456f828b10de
KASH <kashvdmail@gmail.com>
2019-11-23 22:27:39 +0100

    object loading

--- resources/[qb]/qb-interior/client/shells.lua
+++ resources/[qb]/qb-interior/client/shells.lua
@@ -3,7 +3,10 @@ function CreateHotel(spawn)
 
     local POIOffsets = {}
     POIOffsets.exit = json.decode('{"z":2.5,"y":-15.901171875,"x":4.251012802124,"h":2.2633972168}')
-    
+    RequestModel(`playerhouse_hotel`)
+	while not HasModelLoaded(`playerhouse_hotel`) do
+	    Citizen.Wait(1000)
+	end
     local shell = CreateObject(`playerhouse_hotel`, spawn.x, spawn.y, spawn.z, false, false, false)
     FreezeEntityPosition(shell, true)
     table.insert(objects, shell)
@@ -27,7 +30,10 @@ function CreateTier1House(spawn, isBackdoor)
 	POIOffsets.stash = json.decode('{"z":2.5,"y":1.33868212,"x":-9.084908691,"h":2.2633972168}')
 	POIOffsets.logout = json.decode('{"z":2.0,"y":-1.1463337,"x":-6.69117089,"h":2.2633972168}')
     POIOffsets.backdoor = json.decode('{"z":2.5,"y":4.3798828125,"x":0.88999176025391,"h":182.2633972168}')
-
+    RequestModel(`playerhouse_tier1`)
+	while not HasModelLoaded(`playerhouse_tier1`) do
+	    Citizen.Wait(1000)
+	end
     local shell = CreateObject(`playerhouse_tier1`, spawn.x, spawn.y, spawn.z, false, false, false)
     FreezeEntityPosition(shell, true)
     table.insert(objects, shell)
@@ -53,7 +59,10 @@ function CreateTier2House(spawn, isBackdoor)
 
     local POIOffsets = {}
     POIOffsets.exit = json.decode('{"z":2.5,"y":-15.901171875,"x":4.251012802124}')
-
+    RequestModel(`playerhouse_tier1`)
+	while not HasModelLoaded(`playerhouse_tier1`) do
+	    Citizen.Wait(1000)
+	end
     local shell = CreateObject(`playerhouse_tier1`, spawn.x, spawn.y, spawn.z, false, false, false)
     FreezeEntityPosition(shell, true)
 
@@ -78,10 +87,16 @@ function CreateTier3House(spawn, isBackdoor)
     local POIOffsets = {}
     POIOffsets.exit = json.decode('{"y":7.7457427978516,"z":7.2074546813965,"x":-17.097534179688}')
     POIOffsets.backdoor = json.decode('{"z":5.8048210144043,"y":12.009414672852,"x":12.690063476563}')
-
+    RequestModel(`playerhouse_tier3`)
+	while not HasModelLoaded(`playerhouse_tier3`) do
+	    Citizen.Wait(1000)
+	end
     local shell = CreateObject(`playerhouse_tier3`, spawn.x, spawn.y, spawn.z, false, false, false)
     table.insert(objects, shell)
-
+    RequestModel(`v_16_high_lng_over_shadow`)
+	while not HasModelLoaded(`v_16_high_lng_over_shadow`) do
+	    Citizen.Wait(1000)
+	end
     local windows1 = CreateObject(`v_16_high_lng_over_shadow`, spawn.x + 10.16043000, spawn.y + -4.83294600, spawn.z + 4.99192700, false, false, false)
     table.insert(objects, windows1)
 
