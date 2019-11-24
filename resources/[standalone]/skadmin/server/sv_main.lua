e7c69d6a154ec69d9533279ca211c60d44c86634
KASH <kashvdmail@gmail.com>
2019-11-24 22:37:00 +0100

    skadmin weggoneloesoe

--- resources/[standalone]/skadmin/server/sv_main.lua
+++
@@ -1,9 +0,0 @@
-Citizen.CreateThread(function()
-  local dir = Config.settings.saves_directory
-  local banfile = Config.settings.banlist_file
-  if not DATA:fileExists(dir .. banfile) then
-      DATA:createFile(dir .. banfile)
-  end
-
-  print("\n\nSkAdmin Running...\n\n")
-end)
