66e2e3f0830a9c255dc6199bafa70941c8e8d37a
KASH <kashvdmail@gmail.com>
2019-11-27 19:32:35 +0100

    Scrapyard done

---
+++ resources/[qb]/qb-scrapyard/config.lua
@@ -0,0 +1,73 @@
+Config = {}
+
+Config.Locations = {
+    [1] = {
+        ["main"] = {x = 2397.42, y = 3089.44, z = 49.92},
+        ["deliver"] = {x = 2351.5, y = 3132.96, z = 48.2},
+        ["list"] = {x = 2403.51, y = 3127.95, z = 48.15},
+        ["search"] = {
+            [1] = {x = 2413.18, y = 3120.08, z = 48.37},
+            [2] = {x = 2376.81, y = 3090.89, z = 48.45},
+            [3] = {x = 2363.66, y = 3064.18, z = 48.15},
+        },
+    }
+}
+
+Config.Rewards = {
+    [1] = {
+        ["metalscrap"] = math.random(4, 30),
+        ["plastic"] = math.random(4, 30),
+        ["copper"] = math.random(4, 11),
+    },
+    [2] = {
+        ["metalscrap"] = math.random(10, 30),
+        ["plastic"] = math.random(1, 20),
+    },
+    [3] = {
+        ["plastic"] = math.random(5, 25),
+        ["copper"] = math.random(1, 15),
+    },
+    [4] = {
+        ["metalscrap"] = math.random(10, 35),
+        ["copper"] = math.random(1, 15),
+    },
+}
+
+Config.CurrentVehicles = {}
+
+Config.Vehicles = {
+    [1] = "ninef",
+    [2] = "ninef2",
+    [3] = "banshee2",
+    [4] = "alpha",
+    [5] = "baller", 
+    [6] = "baller2", 
+    [7] = "huntley", 
+    [8] = "xls", 
+    [9] = "asea", 
+    [10] = "pigalle",
+    [11] = "sultanrs", 
+    [12] = "turismor", 
+    [13] = "zentorno", 
+    [14] = "dominator",
+    [15] = "blade",
+    [16] = "chino",
+    [17] = "sabregt",
+    [18] = "bati",
+    [19] = "carbonrs",
+    [20] = "akuma",
+    [21] = "thrust",
+    [22] = "exemplar",
+    [23] = "felon",
+    [24] = "sentinel",
+    [25] = "blista",
+    [26] = "fusilade",
+    [27] = "jackal",
+    [28] = "blista2",
+    [29] = "serrano", 
+    [30] = "seminole", 
+    [31] = "tornado",
+    [32] = "picador",
+    [33] = "sadler", 
+    [34] = "bobcatxl", 
+}
