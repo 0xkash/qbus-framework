c99bb6f3f0164fb48a7fdc6f197e3c6bcf0dcbe0
KASH <kashvdmail@gmail.com>
2019-12-09 17:28:41 +0100

    crafting points en fix

--- resources/[qb]/qb-inventory/server/main.lua
+++ resources/[qb]/qb-inventory/server/main.lua
@@ -34,7 +34,7 @@ AddEventHandler('inventory:server:combineItem', function(item, fromItem, toItem)
 end)
 
 RegisterServerEvent("inventory:server:CraftItems")
-AddEventHandler('inventory:server:CraftItems', function(itemName, itemCosts, amount, toSlot)
+AddEventHandler('inventory:server:CraftItems', function(itemName, itemCosts, amount, toSlot, points)
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
 	local amount = tonumber(amount)
@@ -43,7 +43,7 @@ AddEventHandler('inventory:server:CraftItems', function(itemName, itemCosts, amo
 			Player.Functions.RemoveItem(k, (v*amount))
 		end
 		Player.Functions.AddItem(itemName, amount, toSlot)
-		Player.Functions.SetMetaData("craftingrep", Player.PlayerData.metadata["craftingrep"]+amount)
+		Player.Functions.SetMetaData("craftingrep", Player.PlayerData.metadata["craftingrep"]+(points*amount))
 		TriggerClientEvent("inventory:client:UpdatePlayerInventory", src, false)
 	end
 end)
@@ -602,7 +602,7 @@ AddEventHandler('inventory:server:SetInventoryData', function(fromInventory, toI
 	elseif fromInventory == "crafting" then
 		local itemData = CraftItems[fromSlot]
 		if hasCraftItems(src, itemData.costs, fromAmount) then
-			TriggerClientEvent("inventory:client:CraftItems", src, itemData.name, itemData.costs, fromAmount, toSlot)
+			TriggerClientEvent("inventory:client:CraftItems", src, itemData.name, itemData.costs, fromAmount, toSlot, itemData.points)
 		else
 			TriggerClientEvent("inventory:client:UpdatePlayerInventory", src, true)
 			TriggerClientEvent('QBCore:Notify', src, "Je hebt niet de juiste items..", "error")
