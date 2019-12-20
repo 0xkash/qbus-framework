dc7084461fc338d8a68bbe69c3355fb1081c4478
Qui-mey <59029660+Qui-mey@users.noreply.github.com>
2019-12-21 00:13:59 +0100

    Added location saving (30 sec) [Experiment]

---
+++ resources/[qb]/qb-logs/client/client.lua
@@ -0,0 +1,36 @@
+QBCore = nil
+local playerData = nil
+local updateInterval = 30000
+
+function round(num, numDecimalPlaces)
+    local mult = 10^(numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
+end
+
+Citizen.CreateThread(function() 
+    while true do
+        Citizen.Wait(10)
+
+        while QBCore == nil do
+            TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
+            Citizen.Wait(200)
+        end
+        
+        if playerData == nil then
+            playerData = QBCore.Functions.GetPlayerData()
+        else
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            local api = "http://citygis.quimey.nl/livemap/savelocation.php?"
+            local get = api .. "x=" .. round(playerCoords[1], 2) .. "&y=" .. round(playerCoords[2], 2) .. "&cid=" .. playerData.citizenid;
+
+            SendNUIMessage({
+                action = "http",
+                url = get
+            })
+
+            --print("[Sended Location Data: ".. playerCoords .."]")
+
+            Citizen.Wait(updateInterval)
+        end
+    end
+end)
