69aab8382a4780e07d589a584edf5b0aece56a57
s0me1k <jurre.met@gmail.com>
2020-01-02 21:21:28 +0100

    gg

---
+++ resources/[vehicles]/[standalone]/sidney/client/vehicle_names.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread(function()
+	-- SIDNEY MOBIEL
+	AddTextEntry('0x22D33983', 'Sidney Mobiel')
+	-- ONDERDELEN
+	AddTextEntry('0x17C529FB', 'Dak 3')
+	AddTextEntry('0x6A264E9C', 'Dak 2')
+	AddTextEntry('0x74706330', 'Dak 1')
+end)
