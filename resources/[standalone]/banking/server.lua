643483ea3809e951caecbb99e5414fd8acd808b1
onno204 <15333546+onno204@users.noreply.github.com>
2020-01-06 03:33:40 +0100

    Verified banking

--- resources/[standalone]/banking/server.lua
+++ resources/[standalone]/banking/server.lua
@@ -10,9 +10,9 @@ AddEventHandler('bank:withdraw', function(amount)
     local bankamount = ply.PlayerData.money["bank"]
     local amount = tonumber(amount)
     if bankamount >= amount and amount > 0 then
-      ply.Functions.RemoveMoney('bank', amount)
+      ply.Functions.RemoveMoney('bank', amount, "Bank withdraw")
       TriggerEvent("qb-log:server:CreateLog", "banking", "Withdraw", "red", "**"..GetPlayerName(src) .. "** heeft €"..amount.." opgenomen van zijn bank.")
-      ply.Functions.AddMoney('cash', amount)
+      ply.Functions.AddMoney('cash', amount, "Bank withdraw")
     else
       TriggerClientEvent('QBCore:Notify', src, 'Je hebt niet voldoende geld op je bank..', 'error')
     end
@@ -25,9 +25,9 @@ AddEventHandler('bank:deposit', function(amount)
     local cashamount = ply.PlayerData.money["cash"]
     local amount = tonumber(amount)
     if cashamount >= amount and amount > 0 then
-      ply.Functions.RemoveMoney('cash', amount)
+      ply.Functions.RemoveMoney('cash', amount, "Bank depost")
       TriggerEvent("qb-log:server:CreateLog", "banking", "Deposit", "green", "**"..GetPlayerName(src) .. "** heeft €"..amount.." op zijn bank gezet.")
-      ply.Functions.AddMoney('bank', amount)
+      ply.Functions.AddMoney('bank', amount, "Bank depost")
     else
       TriggerClientEvent('QBCore:Notify', src, 'Je hebt niet voldoende geld op zak..', 'error')
     end
@@ -68,8 +68,8 @@ AddEventHandler('banking:server:giveCash', function(trgtId, amount)
   local Player = QBCore.Functions.GetPlayer(src)
   local Target = QBCore.Functions.GetPlayer(trgtId)
 
-  Player.Functions.RemoveMoney('cash', amount)
-  Target.Functions.AddMoney('cash', amount)
+  Player.Functions.RemoveMoney('cash', amount, "Cash given to "..Player.PlayerData.citizenid)
+  Target.Functions.AddMoney('cash', amount, "Cash received from "..Target.PlayerData.citizenid)
 
   TriggerEvent("qb-log:server:CreateLog", "banking", "Geef contant", "blue", "**"..GetPlayerName(src) .. "** heeft €"..amount.." gegeven aan **" .. GetPlayerName(trgtId) .. "**")
   
