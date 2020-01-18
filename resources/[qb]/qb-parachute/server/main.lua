28741d46904d58e3b445290f30584fa39470643d
SirMarkNL <mark@debruijnemail.nl>
2020-01-18 01:53:51 +0100

    Remove failscript #yolo

--- resources/[qb]/qb-parachute/server/main.lua
+++
@@ -1,5 +0,0 @@
-QBCore.Functions.CreateUseableItem("parachute", function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-
-    TriggerClientEvent("qb-parachute:client:UseGear", source, true)
-end)
