d6259338e610b833423bb91ae6bb1afce3dcd8ab
Qui-mey <59029660+Qui-mey@users.noreply.github.com>
2020-01-22 19:57:24 +0100

    Removed vehicle unlockig from delete-lazer

--- resources/[qb]/qb-admin/client/main.lua
+++ resources/[qb]/qb-admin/client/main.lua
@@ -670,9 +670,7 @@ Citizen.CreateThread(function()
                 if IsControlJustReleased(0, 38) then
                     -- Set as missionEntity so the object can be remove (Even map objects)
                     SetEntityAsMissionEntity(entity, true, true)
-                    if IsEntityAVehicle(entity) then
-                        SetVehicleDoorsLocked(entity, 1)
-                    end
+                    SetEntityAsNoLongerNeeded(entity)
                     DeleteEntity(entity)
                 end
             -- Only draw of not center of map
