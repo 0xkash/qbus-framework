4e59645ec0247e7c79d30047711acb86fc9b6b4d
SirMarkNL <mark@debruijnemail.nl>
2020-01-25 01:48:23 +0100

    Typo fix

--- resources/[qb]/qb-inventory/client/main.lua
+++ resources/[qb]/qb-inventory/client/main.lua
@@ -466,7 +466,7 @@ AddEventHandler("inventory:client:ShowDriverLicense", function(sourceId, citizen
     local pos = GetEntityCoords(GetPlayerPed(-1), false)
     if (GetDistanceBetweenCoords(pos.x, pos.y, pos.z, sourcePos.x, sourcePos.y, sourcePos.z, true) < 2.0) then
         TriggerEvent('chat:addMessage', {
-            template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>Voornaam:</strong> {1} <br><strong>Achternaam:</strong> {2} <br><strong>Geboortedag:</strong> {3} <br><strong>Rijbwijzen:</strong> {4}</div></div>',
+            template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>Voornaam:</strong> {1} <br><strong>Achternaam:</strong> {2} <br><strong>Geboortedag:</strong> {3} <br><strong>Rijbewijzen:</strong> {4}</div></div>',
             args = {'Rijbewijs', character.firstname, character.lastname, character.birthdate, character.type}
         })
     end
