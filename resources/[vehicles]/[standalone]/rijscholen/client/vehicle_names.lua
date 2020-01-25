52bd42572f07b556048b8aaf354efb2c473e3216
SirMarkNL <mark@debruijnemail.nl>
2020-01-25 21:12:47 +0100

    Replaced driving school

--- resources/[vehicles]/[standalone]/rijscholen/client/vehicle_names.lua
+++ resources/[vehicles]/[standalone]/rijscholen/client/vehicle_names.lua
@@ -1,6 +1,7 @@
 Citizen.CreateThread(function()
 	-- QBUS 
-	AddTextEntry('0xDA888F97', 'Qbus') 
+	AddTextEntry("0xDA888F97", "Rijschool")
 	-- RIJSCHOOLAUTO'S
-	AddTextEntry('0xE59E9B29', 'Audio Q5 DimiDriving')
+	AddTextEntry("0xE59E9B29", "DimiDriving")
+	AddTextEntry("0x6DB6E6CC", "Sacre Bleu")
 end)
