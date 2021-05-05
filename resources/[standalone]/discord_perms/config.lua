5fb8dde7f42006ef266a4e2a1550c2f8e86bba86
Nick van Duijn <47284922+KASH4@users.noreply.github.com>
2021-05-05 21:01:51 +0200

    Update config.lua

--- resources/[standalone]/discord_perms/config.lua
+++ resources/[standalone]/discord_perms/config.lua
@@ -1,9 +1,9 @@
 Config = {
-	DiscordToken = "NjUwNTAyMTUxOTk4NjY4ODEx.XeMRdw.X4vUFkKBTPpGT9vJVrZyfSOfZ9U",
-	GuildId = "625659298357444619",
+	DiscordToken = "",
+	GuildId = "",
 
 	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
 	Roles = {
-		["Tester"] = "650405109985050635" -- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
+		
 	}
 }
